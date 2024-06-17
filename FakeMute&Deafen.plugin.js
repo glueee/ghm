5c51bea1e1ea3e485b0edeecd9a6066d13325186
pomung <105919824+glueee@users.noreply.github.com>
2024-06-17 18:53:30 +0700

    Updat.js

--- FakeMute&Deafen.plugin.js
+++ FakeMute&Deafen.plugin.js
@@ -1,39 +1,30 @@
-//META{"name":"ExampleLibraryPlugin", "authorId":"575933571186032641", "invite":"TQ2dePQ", "website":"https://github.com/ali0sam"}*//
-//import {DiscordModules as Modules} from "modules";
-class ExampleLibraryPlugin {
+/**
+ * @name FakeMute&Deafen
+ * @version 1
+ * @description Allows you to fake mute and deafen yourself, shows for others. This has been found by many others but I've decided to add some features to beautify it.
+ * @author Shifts
+ * @source https://github.com/ignshifts
+ */
+
+module.exports = class FakeMuteDeafen {
+  start() {
+
+    let React = BdApi.React;
+    let Patcher = BdApi.Patcher;
     
-    getName() {return "Fake Mute&Deafen";}
-    getDescription() {return "Join voice channel, mute and deafen yourself, start and stop plugin, now you can Un-mute and Listen and speak!";}
-    getVersion() {return "0.0.1";}
-    getAuthor() {return "ali_s";}
-    
+    var text = new TextDecoder("utf-8");
 
-
-
-    start() {
-        if (!global.ZeresPluginLibrary) return window.BdApi.alert("Library Missing",`The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
-        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "LINK_TO_RAW_CODE");
-        
-        var text = new TextDecoder("utf-8");
-
-WebSocket.prototype.original = WebSocket.prototype.send;
-WebSocket.prototype.send = function(data) {
-    if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
+    WebSocket.prototype.original = WebSocket.prototype.send;
+    WebSocket.prototype.send = function (data) {
+      if (Object.prototype.toString.call(data) === "[object ArrayBuffer]") {
         if (text.decode(data).includes("self_deaf")) {
-            console.log("found mute/deafen");
-            data = data.replace('"self_mute":false', 'NiceOneDiscord');
-            console.log("Activated");
+          data = data.replace('"self_mute":false');
         }
-    }
-    WebSocket.prototype.original.apply(this, [data]);
-}
-window.BdApi.alert("success",`now you can stop plugin!, After you turned ON the plugin , you cant join any other voice channels , for this you have to Reload discord!`);
-
-    }
-
-
-
-    stop() {
-
-    }
-}
+      }
+      WebSocket.prototype.original.apply(this, [data]);
+    };
+    BdApi.alert("Enabled", `You can now unmute or undeafen yourself and it will show for others. The plugin will be disabled by default.`);
+    BdApi.Plugins.disable('FakeMute&Deafen')
+  }
+  stop() {}
+};
